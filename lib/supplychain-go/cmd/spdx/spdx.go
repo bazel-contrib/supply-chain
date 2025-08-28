c0629bc0f03edd950b69895db4d885be1d48c00a
TheGrizzlyDev <tonio9681@gmail.com>
2025-08-28 17:47:01 +0100

    WIP: generate first sbom

--- lib/supplychain-go/cmd/spdx/spdx.go
+++ lib/supplychain-go/cmd/spdx/spdx.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"os"
 
+	supplychain "github.com/bazel-contrib/supply-chain/lib/supplychain-go"
 	"github.com/bazel-contrib/supply-chain/lib/supplychain-go/internal/sbom"
 	spdxJson "github.com/spdx/tools-golang/json"
 	"github.com/spdx/tools-golang/spdx"
@@ -26,15 +27,35 @@ func main() {
 
 	json.Unmarshal(configBytes, &config)
 
-	doc := spdx.Document{
-		SPDXVersion: "SPDX-3.0",
-	}
-
 	out, err := os.OpenFile(*outPath, os.O_WRONLY|os.O_CREATE, 0664)
 	if err != nil {
 		panic(err)
 	}
 	defer out.Close()
 
-	spdxJson.Write(&doc, out)
+	doc, err := GenerateDocument(config)
+	if err != nil {
+		panic(err)
+	}
+
+	spdxJson.Write(doc, out)
+}
+
+func GenerateDocument(config sbom.GenConfig) (*spdx.Document, error) {
+	spdxPackages := make([]*spdx.Package, len(config.Deps))
+
+	for i, dep := range config.Deps {
+		pkgMetadata, err := supplychain.ReadPackageMetadataFromFile(dep.Metadata)
+		if err != nil {
+			return nil, err
+		}
+		spdxPackages[i] = &spdx.Package{
+			PackageName: pkgMetadata.GetPURL().Name,
+		}
+	}
+
+	return &spdx.Document{
+		SPDXVersion: "SPDX-3.0",
+		Packages:    spdxPackages,
+	}, nil
 }
