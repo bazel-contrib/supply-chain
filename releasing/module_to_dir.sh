9b44274bfdecc7be577395ea2f92ab6a8b9488b9
Tony Aiuto <tony@aiuto.dev>
2026-04-30 14:47:47 -0400

    Update sub-module publishing to add publish to bcr
    Update the publish to bcr from bazel-contrib to handled distinct modules within a repo
    
    The conceptual differences are:
    1) The release tag is no long presumed to be just a version with a v.
       - we explicitly pass the module name, version, and release tag.
       - we could have also done this by passing tag and tag_prefix="$module_name-",
         but that is less readable.
    2) There is a per module set of templates in .bcr
    
    - Copy the workflow script from https://github.com/bazel-contrib/publish-to-bcr
    - Minimal modifications
      - I could eventually try to continue to support multi-publish, but I thin the idea is wrong.
    - Update release_tools to call it
    - Update release_prep to pull the module name out of TAG and use it in URLS
      instead of assuming that repo==module.
    
    This works up to the point I can't test any more.
    https://github.com/aiuto/supply-chain/actions/runs/25187564519
    
    It fails on getting the publish token, which I have not figured out yet.

---
+++ releasing/module_to_dir.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# Map external module name to where it is stored.
+
+MODULE="$1"
+
+declare -A module2dir
+module2dir["package_metadata"]="metadata"
+module2dir["supply-chain-go"]="lib/supplychain-go"
+module2dir["supply_chain_tools"]="tools"
+module2dir["supply_chain_examples"]="examples"
+
+echo "${module2dir[${MODULE}]:-$MODULE}"
+exit 0
